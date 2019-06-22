fe1b5e3dd88ad68285f856892a4b5fbeb7fe91d2
王绍峰 <1693476459@qq.com>
2019-06-22 16:32:48 +0800

    SinleR的第一个Demo

---
+++ SingleRTest/Startup1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SingleRTest.Startup1))]
+
+namespace SingleRTest
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
---
+++ SingleRTest/TestHub.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SingleRTest
+{
+    public class TestHub : Hub
+    {
+        public void SendMessage(string name, string msg)
+        {
+
+            //string name1 = Context.QueryString["username"];
+            //string age = Context.QueryString["age"];
+            Clients.All.onMessage(name + "和大家说" + msg);//调用所有连接上来的客户端（包括自己）监听的onMessage事件。All是一个dynamic属性，所以可以随意的监听
+        }
+
+
+        public override Task OnConnected()
+        {
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            return base.OnDisconnected(stopCalled);
+        }
+
+        public override Task OnReconnected()
+        {
+            return base.OnReconnected();
+        }
+    }
+}
